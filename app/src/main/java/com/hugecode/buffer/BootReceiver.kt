2f7f686231ba1d50fbbe7df625efd637ae1e9b84
SabOrRoblox <maksymorigonal2013@gmail.com>
2026-08-30 18:21:54 +0300

    Add support for HTC quick boot action

--- app/src/main/java/com/hugecode/buffer/BootReceiver.kt
+++ app/src/main/java/com/hugecode/buffer/BootReceiver.kt
@@ -7,7 +7,9 @@ import android.content.Intent
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
-            intent.action == "android.intent.action.QUICKBOOT_POWERON") {
+            intent.action == "android.intent.action.QUICKBOOT_POWERON" ||
+            intent.action == "com.htc.intent.action.QUICKBOOT_POWERON") {
+            
             val serviceIntent = Intent(context, DeviceService::class.java)
             context.startForegroundService(serviceIntent)
         }
