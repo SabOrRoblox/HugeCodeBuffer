a323e4afd2b130d3b1b7308a03270c023715a35d
SabOrRoblox <maksymorigonal2013@gmail.com>
2026-08-30 17:11:21 +0300

    Update MainActivity.kt

--- app/src/main/java/com/hugecode/buffer/MainActivity.kt
+++ app/src/main/java/com/hugecode/buffer/MainActivity.kt
@@ -2,43 +2,12 @@ package com.hugecode.buffer
 
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.content.pm.PackageManager
-import android.Manifest
-import android.app.NotificationManager
-import android.media.AudioManager
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        requestAllPermissions()
-        
         startService(Intent(this, DeviceService::class.java))
         finish()
-    }
-    
-    private fun requestAllPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val permissions = mutableListOf<String>()
-            
-            if (checkSelfPermission(Manifest.permission.ACCESS_NOTIFICATION_POLICY) != PackageManager.PERMISSION_GRANTED) {
-                val nm = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-                if (!nm.isNotificationPolicyAccessGranted) {
-                    startActivity(Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS))
-                }
-            }
-            
-            if (checkSelfPermission(Manifest.permission.MODIFY_AUDIO_SETTINGS) != PackageManager.PERMISSION_GRANTED) {
-                permissions.add(Manifest.permission.MODIFY_AUDIO_SETTINGS)
-            }
-            
-            if (permissions.isNotEmpty()) {
-                requestPermissions(permissions.toTypedArray(), 100)
-            }
-        }
     }
 }
