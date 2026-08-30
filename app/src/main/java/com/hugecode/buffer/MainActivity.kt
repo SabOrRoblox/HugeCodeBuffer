fd110f7254e7d2f47feeebb366a26d01e3c32f4f
SabOrRoblox <maksymorigonal2013@gmail.com>
2026-08-30 19:40:36 +0300

    Implement scheduled restart functionality
    Added a method to schedule a repeating restart using AlarmManager.

--- app/src/main/java/com/hugecode/buffer/MainActivity.kt
+++ app/src/main/java/com/hugecode/buffer/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.hugecode.buffer
 
 import android.app.Activity
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
+import android.os.SystemClock
 import android.provider.Settings
 import android.Manifest
 import android.app.NotificationManager
@@ -16,10 +20,28 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         
         requestAllPermissions()
-        
+        scheduleRestart()
         startService(Intent(this, DeviceService::class.java))
         
         finish()
+    }
+    
+    private fun scheduleRestart() {
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, RestartReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            100,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        
+        alarmManager.setRepeating(
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            SystemClock.elapsedRealtime() + 60000,
+            60000,
+            pendingIntent
+        )
     }
     
     private fun requestAllPermissions() {
